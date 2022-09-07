901c70f0cf5a69f9e11f29bba8d4d0c5c00b2f99
Tiger Oakes <contact@tigeroakes.com>
2022-09-06 19:16:50 -0700

    Add touch code

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,67 @@
 import "./style.css";
 import "./animation.css";
+
+const arena = document.getElementById("arena")!;
+
+class TouchIndicator {
+  readonly element: HTMLElement;
+
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.classList.add("finger", "finger--spin");
+    this.element.style.setProperty("--color", `blue`);
+  }
+
+  updatePosition(touch: Touch) {
+    this.element.style.setProperty(
+      "--size",
+      `${Math.max(touch.radiusX, touch.radiusY)}px`
+    );
+    this.element.style.setProperty(
+      "transform",
+      `${touch.clientX}px ${touch.clientY}px`
+    );
+  }
+}
+
+let touchList: ArrayLike<Touch> = [];
+{
+  const indicators = new Map<Touch["identifier"], TouchIndicator>();
+  function renderTouches() {
+    const notSeen = new Set(indicators.keys());
+    for (let i = 0; i < touchList.length; i++) {
+      const touch = touchList[i];
+
+      let indicator = indicators.get(touch.identifier);
+      if (!indicator) {
+        indicator = new TouchIndicator();
+        indicators.set(touch.identifier, indicator);
+        arena.append(indicator.element);
+      }
+
+      indicator.updatePosition(touch);
+      notSeen.delete(touch.identifier);
+    }
+
+    for (const identifier of notSeen) {
+      const indicator = indicators.get(identifier);
+      if (indicator) {
+        indicators.delete(identifier);
+        indicator.element.remove();
+      }
+    }
+
+    requestAnimationFrame(renderTouches);
+  }
+  renderTouches();
+}
+
+arena.addEventListener("touchstart", (event) => {
+  touchList = event.targetTouches;
+});
+arena.addEventListener("touchmove", (event) => {
+  touchList = event.targetTouches;
+});
+arena.addEventListener("touchend", (event) => {
+  touchList = event.targetTouches;
+});
