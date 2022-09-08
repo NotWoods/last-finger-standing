d521ee8e28c3d3ee8ee92acb7aeb5631261cd0c8
Tiger Oakes <contact@tigeroakes.com>
2022-09-07 19:14:36 -0700

    Build script update

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
+  build: {
+    rollupOptions: {
+      input: {
+        landing: "index.html",
+        app: "app.html",
+      },
+    },
+  },
 });
