28d44ada7039f9370e1303927734f8eeba5c9093
Tiger Oakes <contact@tigeroakes.com>
2022-09-27 20:10:53 -0700

    Add wake lock

--- src/fullscreen.ts
+++ src/fullscreen.ts
@@ -1,21 +1,43 @@
-const fullscreenButton = document.querySelector(".fullscreen-button")!;
+{
+  const fullscreenButton = document.querySelector(".fullscreen-button")!;
 
-fullscreenButton.addEventListener("click", () => {
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    document.documentElement.requestFullscreen();
+  fullscreenButton.addEventListener("click", () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  });
+
+  document.addEventListener("fullscreenchange", () => {
+    fullscreenButton.textContent = document.fullscreenElement
+      ? "Exit fullscreen"
+      : "Fullscreen";
+    fullscreenButton.setAttribute(
+      "aria-pressed",
+      document.fullscreenElement ? "true" : "false"
+    );
+  });
+}
+
+if ("wakeLock" in navigator) {
+  let wakeLock: WakeLockSentinel | undefined;
+
+  async function requestWakeLock() {
+    try {
+      wakeLock = await navigator.wakeLock.request("screen");
+    } catch (err) {
+      if (!(err instanceof Error)) throw err;
+      console.error(`${err.name}, ${err.message}`);
+    }
   }
-});
 
-document.addEventListener("fullscreenchange", () => {
-  fullscreenButton.textContent = document.fullscreenElement
-    ? "Exit fullscreen"
-    : "Fullscreen";
-  fullscreenButton.setAttribute(
-    "aria-pressed",
-    document.fullscreenElement ? "true" : "false"
-  );
-});
+  requestWakeLock();
+  document.addEventListener("visibilitychange", () => {
+    if (!wakeLock && document.visibilityState === "visible") {
+      requestWakeLock();
+    }
+  });
+}
 
 export {};
