07386e236a78eaab2de6138ae1c82b45f9a87747
Tiger Oakes <contact@tigeroakes.com>
2022-11-14 08:15:15 +0000

    Fix published files

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,14 @@
+// @ts-check
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: '/last-finger-standing/',
+  base: "/last-finger-standing/",
   server: {
     host: "0.0.0.0",
   },
   build: {
+    outDir: "dist/last-finger-standing",
     rollupOptions: {
       input: {
         landing: "index.html",
